64bc12387c63418b8435d156477ab4e39faa31bc
Lisa Koss <lisakoss@uw.edu>
2018-01-24 01:02:37 -0800

    Create basic add, subtract, multiply, divide funcs

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,20 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
 }
